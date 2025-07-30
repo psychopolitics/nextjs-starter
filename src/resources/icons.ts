48e7ce2e3794f317526c6043fe7ed01be1de4cf7
psychopolitics <psychopolitics@icloud.com>
2025-07-30 07:02:44 -0700

    Update icons.ts

--- src/resources/icons.ts
+++ src/resources/icons.ts
@@ -1,12 +1,27 @@
+// icons.ts
 import { IconType } from "react-icons";
-
-import {
-  HiOutlineRocketLaunch,
-} from "react-icons/hi2";
-
+import { PiHouse, PiUser, PiGridFour, PiBookBookmark, PiImage, PiEnvelope, PiGlobe, PiLink, PiArrowRight, PiArrowUpRight, PiX, PiDotsThree, PiCode, PiTerminal } from "react-icons/pi";
+import { FaGithub, FaLinkedin, FaDiscord, FaThreads } from "react-icons/fa6";
 
 export const iconLibrary: Record<string, IconType> = {
-  rocket: HiOutlineRocketLaunch,
+  home: PiHouse,
+  person: PiUser,
+  grid: PiGridFour,
+  book: PiBookBookmark,
+  gallery: PiImage,
+  link: PiLink,
+  arrowRight: PiArrowRight,
+  arrowUpRight: PiArrowUpRight,
+  dots: PiDotsThree,
+  email: PiEnvelope,
+  globe: PiGlobe,
+  github: FaGithub,
+  linkedin: FaLinkedin,
+  discord: FaDiscord,
+  threads: FaThreads,
+  x: PiX,
+  code: PiCode,
+  terminal: PiTerminal,
 };
 
 export type IconLibrary = typeof iconLibrary;
